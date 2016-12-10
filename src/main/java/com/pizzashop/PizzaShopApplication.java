c96caad4b2839508da92113a319d821886a284cb
bartoszPigla <bartoszpigla@o2.pl>
2016-12-10 21:32:10 +0100

    psogresql config changed

--- src/main/java/com/pizzashop/PizzaShopApplication.java
+++ src/main/java/com/pizzashop/PizzaShopApplication.java
@@ -8,12 +8,11 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.*;
 import org.springframework.core.Ordered;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
@@ -23,6 +22,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 import java.util.List;
 
 @SpringBootApplication
@@ -34,6 +34,14 @@ public class PizzaShopApplication extends WebMvcConfigurerAdapter{
 		SpringApplication.run(PizzaShopApplication.class, args);
 
 	}
+
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
+
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/addProduct").setViewName("addProduct.html");
